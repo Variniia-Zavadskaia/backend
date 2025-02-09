915e7fbd9c51020f452892997e5e06c665e094c5
Variniia Zavadskaia <variniyaal@gmail.com>
2025-02-09 23:30:23 +0700

    build

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,5 @@
-export default  {
-  dbURL: 'mongodb://127.0.0.1:27017',
-  dbName : 'insta_db'
+export default {
+    //   dbURL: 'mongodb://127.0.0.1:27017',
+    dbURL: 'mongodb+srv://insta_proj:a1pCpIjdSEVcdsLy@cluster0.i4vkq.mongodb.net/',
+    dbName: 'insta_db',
 }
