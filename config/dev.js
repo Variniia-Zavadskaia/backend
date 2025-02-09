eb62d3d1237e5d016042ba86a3ec79c7561ebec0
Variniia Zavadskaia <variniyaal@gmail.com>
2025-02-10 00:30:54 +0700

    cover mongodb path

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,4 @@
 export default {
-    //   dbURL: 'mongodb://127.0.0.1:27017',
-    dbURL: 'mongodb+srv://insta_proj:a1pCpIjdSEVcdsLy@cluster0.i4vkq.mongodb.net/',
+    dbURL: 'mongodb://127.0.0.1:27017',
     dbName: 'insta_db',
 }
--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,12 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://insta_proj:a1pCpIjdSEVcdsLy@cluster0.i4vkq.mongodb.net/',
-  dbName : process.env.DB_NAME || 'insta_db'
+  dbURL: process.env.MONGO_URL,
+  dbName : process.env.DB_NAME
 }
+
+// export default {
+//     dbURL: process.env.MONGO_URL || 'mongodb+srv://insta_proj:a1pCpIjdSEVcdsLy@cluster0.i4vkq.mongodb.net/',
+//     dbName : process.env.DB_NAME || 'insta_db'
+//   }
